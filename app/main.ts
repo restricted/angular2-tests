aa403038b90d5b56a612e2f47a0dfd5239fcc648
Dementyev Alexey <adementyev@zaoblako.ru>
2016-08-09 14:06:34 +0300

    feat: add angular quickstart

---
+++ app/main.ts
@@ -0,0 +1,11 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { enableProdMode } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+
+
+enableProdMode();
+
+bootstrap(AppComponent);
